c99c1982a88bff0c735816304b617085937221a1
Success Hycenth <successhycenth8@gmail.com>
2021-08-05 03:59:06 -0400

    updating website

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,21 +1,15 @@
 /** @format */
 import { IoMdArrowRoundForward } from "react-icons/io";
-import { useEffect } from "react";
 import CustomButton from "../custom-button/custom-button";
-import Aos from "aos";
 import { Link } from "react-router-dom";
-import "aos/dist/aos.css";
 
 import "./home.scss";
 
 const Main = () => {
-  useEffect(() => {
-    Aos.init({ duration: 1000, offset: 150 });
-  }, []);
   return (
     <div className="main">
       <section className="section-1">
-        <section className="glass-wrapper" data-aos="fade-up">
+        <section className="glass-wrapper">
           <div className="me">
             <h1>
               <span>Hi, I'm Success Hycenth.</span>I'm a Web Developer and UI/UX
